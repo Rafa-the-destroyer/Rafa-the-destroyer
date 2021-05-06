ff1380fddf10c79a85a87998f597ccca4cc618fd
Rafael Limão <63013454+Rafa-the-destroyer@users.noreply.github.com>
2021-05-06 18:46:10 -0300

    Fixed styling Header error

--- site/_includes/header.js
+++ site/_includes/header.js
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div>
+            <div className={styles.headerLink}>
                 <a href='/'>
                     <h2>Rafaelimao</h2>
                 </a>
